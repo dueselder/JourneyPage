c709626798abfe4f3ef89c509676d50c60e837f7
Christopher Düselder <c.dueselder@live.de>
2024-02-01 17:19:38 +0100

    Refactor Card component markup

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -6,8 +6,8 @@ function Card(props) {
     <div className="card">
       <img src={`./images/${props.item.imageUrl}`} alt={props.item.title} className="card-image"/>
       <div className="card-text">
-        <h2>{props.item.location}</h2>
-        <h3>{props.item.title}</h3>
+        <p>{props.item.location}</p>
+        <h2>{props.item.title}</h2>
         <p>{props.item.startDate} - {props.item.endDate}</p>
         <a href={props.item.googleMapsUrl}>Google Maps Link</a>
         <p>{props.item.description}</p>
